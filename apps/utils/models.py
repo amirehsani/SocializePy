d21b2f56e98a4cd3c16d1fd574c965abf2f2766f
Amir Ehsani <amirmohammad7284@gmail.comm>
2023-09-15 16:58:30 +0330

    Adding 'utils' app + setting API docs

---
+++ apps/utils/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.db.models.query import F, Q
+from django.utils import timezone
+
+
+class BaseModel(models.Model):
+
+    created_at = models.DateTimeField(db_index=True, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class RandomModel(BaseModel):
+
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="start_date_before_end_date",
+                check=Q(start_date__lt=F("end_date"))
+            )
+        ]

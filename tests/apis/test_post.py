4b3b521b107d71295328dbdd2fa3803dcf1ade5e
Amir Ehsani <amirmohammad7284@gmail.comm>
2023-09-16 15:02:35 +0330

    Adding tests

---
+++ tests/apis/test_post.py
@@ -0,0 +1,15 @@
+import pytest
+import json
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_empty_post_api(api_client, user1, subscription1, profile1, post1):
+    url_ = reverse("api:blog:post")
+
+    response = api_client.get(url_, content_type="application/json")
+    data = json.loads(response.content)
+
+    assert response.status_code == 200
+    assert data.get('results') == []
+    assert data.get('limit') == 10
